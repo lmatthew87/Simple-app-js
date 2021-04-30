549eb91227454a0c6696d11001e57229acb5116e
lmatthew87 <81183261+lmatthew87@users.noreply.github.com>
2021-04-30 11:31:50 -0700

    Updated pokedex
    Changed sizes

---
+++ JS/scripts.js
@@ -0,0 +1,17 @@
+let pokemonList = [
+  {
+    name: "Bulbasaur",
+    types: ['grass', 'poison'],
+    height: .7
+  },
+  {
+    name: "Pikachu",
+    types: ['electric'],
+    height: .4
+  },
+  {
+    name: "Onix",
+    types: ['ground', 'rock'],
+    height: 8.8
+  }
+];
