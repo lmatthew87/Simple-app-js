136793c4a7ceacb3e7c845c6fbde9ae342a39f2d
lmatthew87 <81183261+lmatthew87@users.noreply.github.com>
2021-05-31 09:44:48 -0700

    Added loop

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,12 +2,12 @@ let pokemonList = [
   {
     name: "Bulbasaur",
     types: ['grass', 'poison'],
-    height: .7
+    height: 7
   },
   {
     name: "Pikachu",
     types: ['electric'],
-    height: .4
+    height: 4
   },
   {
     name: "Onix",
@@ -15,3 +15,10 @@ let pokemonList = [
     height: 8.8
   }
 ];
+for pokemonList = [
+  {
+    document.write(
+      “Bulbasaur (height: 7)”
+    )
+  }
+]
