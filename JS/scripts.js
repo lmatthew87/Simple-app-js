c96458b623153d0a4d6f434f270ca77b2fffc729
lmatthew87 <81183261+lmatthew87@users.noreply.github.com>
2021-06-11 14:03:31 -0700

    Fixed loops properly
    Displays all correct information

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,12 +2,12 @@ let pokemonList = [
   {
     name: "Bulbasaur",
     types: ['grass', 'poison'],
-    height: 7
+    height: .7
   },
   {
     name: "Pikachu",
     types: ['electric'],
-    height: 4
+    height: .4
   },
   {
     name: "Onix",
@@ -15,10 +15,16 @@ let pokemonList = [
     height: 8.8
   }
 ];
-for pokemonList = [
-  {
-    document.write(
-      “Bulbasaur (height: 7)”
-    )
+for (let i = 0; i < pokemonList.length; i++){
+
+  document.write(pokemonList[i].name)
+  document.write(" (height: ")
+  document.write(pokemonList[i].height)
+  document.write(")")
+
+  if(pokemonList[i].height>1){
+    document.write(" Wow that's big")
   }
-]
+
+  document.write("<br>")
+}
